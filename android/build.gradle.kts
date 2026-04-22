f8ba19865d5f6cd7983136a57737fb1094bf372d
Vegiko <121432567+Vegiko@users.noreply.github.com>
2026-04-22 11:54:15 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false 
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
 allprojects {
     repositories {
