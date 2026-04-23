17b2ba96940bd91147ed8c0782457329135b3d5a
Vegiko <121432567+Vegiko@users.noreply.github.com>
2026-04-23 14:51:52 +0100

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,9 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android") 
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
-
-
 android {
     namespace = "com.example.tourism_app"
     compileSdk = flutter.compileSdkVersion
