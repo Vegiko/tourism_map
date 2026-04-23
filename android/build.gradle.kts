2cad7d4fcba2d879abeba70652389a07b7e6255b
Vegiko <121432567+Vegiko@users.noreply.github.com>
2026-04-23 14:36:06 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") apply false
     id("com.android.library") apply false
     id("org.jetbrains.kotlin.android") version "2.1.0"  apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
 allprojects {
     repositories {
